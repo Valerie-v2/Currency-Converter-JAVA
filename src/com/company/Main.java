bdeb26c54b084c88c31b0d5110fb4499fb032600
Witteveen van, Valerie - Student Rijn IJssel <00246262@rijnijssel.nl>
2021-11-25 13:31:12 +0100

    while loop weggehaald

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
+
 import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void clearScreen() {
+    public static void clearScreen() { // Om het console scherm schoon te vegen, maar werkt niet lol
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-    public static void readAndClear(){
+
+    public static void readAndClear() { // Om na het converten terug te keren naar het begin van het programma
         Scanner scan = new Scanner(System.in);
         System.out.println("\n\n");
         System.out.println("Press enter to return to start...");
@@ -17,10 +19,11 @@ public class Main {
         System.out.flush();
         scan.nextLine();
     }
+
     public static void main(String[] args) {
         //StandardCharsets charset = new StandardCharsets();
         Scanner scan = new Scanner(System.in);
-        CharacterSymbols charSymbols = new CharacterSymbols();
+        CharacterSymbols Char = new CharacterSymbols(); // De symbolen voor de munteenheid
         String currencyGivenByUser;
         double amountGivenByUser;
         String convertedCurrency;
@@ -30,15 +33,8 @@ public class Main {
             System.out.println("WELCOME TO THE CURRENCY CONVERTER! *JAVA*");
             System.out.println("Please insert the current currency (Euro, Pound, Dollar, Yen, Bitcoin): ");
             currencyGivenByUser = scan.nextLine().toLowerCase();
-            do {
-                System.out.println("Please insert the amount of money you want to convert: ");
-                while (!scan.hasNextInt()) {
-                    System.out.println("Please insert numbers only!");
-                }
-                amountGivenByUser = scan.nextInt();
-            } while (amountGivenByUser > 0);
-
-            System.out.println("What is the currency you want to convert to? (Euro, Pound, Dollar, Yen, Bitcoin): ");
+            System.out.println("Please insert the amount of money you want to convert: ");
+            //todo try catch of while loop voor INT check
         }
     }
 }
